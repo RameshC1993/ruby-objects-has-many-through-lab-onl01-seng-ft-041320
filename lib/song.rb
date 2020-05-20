fbf8b832d6d10c080c771004688232e64233389f
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-05-20 22:36:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,7 @@
 class Song 
-
-  attr_accessor :name, :artist, :genre
   @@all = []
-
+  attr_accessor :name, :artist, :genre
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
